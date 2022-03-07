b5c6d270b110cff7b437447cc087ad1b78116bbb
Дмитрий Андреев <dimitrius.nedraw@gmail.com>
2022-03-07 15:54:19 +0700

    add address model

--- apihh/methods/vacancy/__init__.py
+++ apihh/methods/vacancy/__init__.py
@@ -1,6 +1,7 @@
+from datetime import datetime
+from typing import List
 from requests import Session
 import json
-from .responses import Vacancy
 
 
 class VacancyRequester:
@@ -9,10 +10,41 @@ class VacancyRequester:
         self.__session = session
 
     def get_vacancy(self, id:str):
-        method = 'vacancies/'
-        url_path = self.__BASE_URL + method + id
-        response = self.__session.get(url_path)
-        response = json.loads(response.text)
-        response = Vacancy.parse_obj(response)
-                  
-        return response
+        pass
+
+
+    def get_vacancies(
+        text: str | None = None,
+        search_fields: List[str] | None = None,
+        experience: str | None = None,
+        employment: str | None = None,
+        schedule: str | None = None,
+        area: str | None = None,
+        metro: str | None = None,
+        industry: str | None = None,
+        employers_id: List[str] | None = None,
+        currency: str | None = None,
+        salary: int | None = None,
+        only_with_salary: bool | None = None,
+        label: List[str] | None = None,
+        period: int | None = None,
+        date_from: datetime | None = None,
+        date_to: datetime | None = None,
+        top_lat: float | None = None,
+        bottom_lat: float | None = None,
+        left_lng: float | None = None,
+        right_lng: float | None = None,
+        order_by: str | None = None,
+        sort_point_lat: float | None = None,
+        sort_point_lng: float | None = None,
+        clusters: bool | None = None,
+        describe_arguments: bool | None = None,
+        per_page: int | None = None,
+        page: int | None = None,
+        no_magic:bool | None = None,
+        premium: bool | None = None,
+        responses_count_enabled: bool | None = None,
+        part_time: List[str] | None = None,
+        professional_role: List[str] | None = None
+    ):
+        pass
--- apihh/models.py
+++ apihh/models.py
@@ -2,18 +2,7 @@ from typing import List
 from pydantic import BaseModel, Field
 
 
-class Skill(BaseModel):
-    name: str
-
-class Schedule(BaseModel):
-    id: str
-    name: str
-
-class Experience(BaseModel):
-    id: str
-    name: str
-
-class MetroStations(BaseModel):
+class MetroStation(BaseModel):
     station_id: str
     station_name: str
     line_id: str
@@ -28,105 +17,8 @@ class Address(BaseModel):
     description: str | None
     lat: float | None
     lng: float | None
-    metro_stations: List[MetroStations]
+    metro_stations: List[MetroStation]
 
-class Department(BaseModel):
-    id: str
-    name: str
-
-class Employment(BaseModel):
-    id: str
-    name: str
-
-class Salary(BaseModel):
-    from_salary: float = Field(default=None, alias='from')
-    to_salary: float = Field(default=None, alias='to')
-    gross: bool | None
-    currency: str
-
-class InsiderInterviewSmall(BaseModel):
-    id: str
-    url: str
-
-class InsiderInterview(InsiderInterviewSmall):
-    title: str
-
-class Area(BaseModel):
-    id: str
-    name: str
-    url: str
-
-class Industry(BaseModel):
-    id: str
-    name: str
-
-class EmployerSmall(BaseModel):
-    logo_urls: dict | None
-    name: str
-    alternate_url: str
-    id: str
-    trusted: bool
-
-
-
-
-
-class Employer(EmployerSmall):
-    
-   
-    type: str | None
-    site_url: str
-    description: str | None
-    branded_description: str | None
-    vacancies_url: str
-    open_vacancies: int
-    
-    
-    insider_interviews: List[InsiderInterview]
-    
-    area: Area
-    relations: List[str]
-    industries: List[Industry]
-
-class VacancyType(BaseModel):
-    id: str
-    name: str
-
-class Test(BaseModel):
-    required: bool
-
-class Contacts(BaseModel):
-    class Phone(BaseModel):
-        country: str
-        city: str
-        number: str
-        comment: str | None
-    name: str | None
-    email: str | None
-    phones: List[Phone]
-
-class BillingType(BaseModel):
-    id: str
-    name: str
-
-class DriverLicenseType(BaseModel):
-    id: str
-
-class WorkingDays(BaseModel):
-    id: str
-    name: str
-
-class WorkingTimeIntervals(BaseModel):
-    id: str
-    name: str
-
-class WorkingTimeModes(BaseModel):
-    id: str
-    name: str
-
-class ProfessionalRole(BaseModel):
-    id: str
-    name: str
 
 
 
